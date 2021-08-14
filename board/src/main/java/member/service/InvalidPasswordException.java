e9aa59432ede7704c871dd9928b66c8c885c12bd
JaeKwang Park <qo010478@gmail.com>
2021-08-14 16:43:27 +0900

    비밀번호 변경 기능 구현(발생 가능한 예외 처치를 위해 익셉션 클래스 작성)

---
+++ board/src/main/java/member/service/InvalidPasswordException.java
@@ -0,0 +1,5 @@
+package member.service;
+
+public class InvalidPasswordException extends RuntimeException{// 비밀번호 변경시 현재 비밀번호가 입력한 것과 다를 때 발생하는 예외
+
+}
---
+++ board/src/main/java/member/service/MemberNotFoundException.java
@@ -0,0 +1,5 @@
+package member.service;
+
+public class MemberNotFoundException extends RuntimeException {// 비밀번호 변경시, 해당 유저를 찾지 못했을 때 발생하는 예외
+
+}
