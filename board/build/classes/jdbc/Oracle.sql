753f3d7e9eae280474a2cafc913181c0ff30db9b
JaeKwang Park <qo010478@gmail.com>
2021-08-16 11:36:24 +0900

    게시판 정보 저장 테이블 관련 쿼리문 작성

--- board/build/classes/jdbc/Oracle.sql
+++ board/build/classes/jdbc/Oracle.sql
@@ -9,3 +9,26 @@ create table member(
 
 select * from member;
 select * from member where memberid = 'Example';
+
+// 게시글 정보 저장 테이블
+create table article(
+	article_no int primary key,
+	writer_id varchar(50) not null,
+	writer_name varchar(50) not null,
+	title varchar(50) not null,
+	regdate timestamp not null,
+	moddate timestamp not null,
+	read_cnt int
+);
+
+// 게시글 번호  자동 증가 시퀀스
+create sequence num_seq;
+
+// 게시글 내용 저장 테이블
+create table article_content (
+	article_no int primary key,
+	content long
+);
+
+select * from article;
+select * from article_content;
