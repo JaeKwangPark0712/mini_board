fd59b9bd1bc261baf65a4fb8677ec6bd80ef2175
JaeKwang Park <qo010478@gmail.com>
2021-08-16 16:48:16 +0900

    게시글 내용 조회 기능 구현(게시글 조회 관련 익셉션 클래스 작성)

---
+++ board/src/main/java/article/service/ArticleContentNotFoundException.java
@@ -0,0 +1,5 @@
+package article.service;
+
+public class ArticleContentNotFoundException extends RuntimeException {// 조회할 게시글 내용을 찾을 수 없을 경우 발생하는 예외
+
+}
---
+++ board/src/main/java/article/service/ArticleNotFoundException.java
@@ -0,0 +1,5 @@
+package article.service;
+
+public class ArticleNotFoundException extends RuntimeException {// 조회 할 게시글 정보를 찾지 못했을 경우 발생하는 예외
+
+}
