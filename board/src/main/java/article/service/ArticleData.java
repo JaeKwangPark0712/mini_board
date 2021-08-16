7441bfd3847a36940659e76360ad19558e3189af
JaeKwang Park <qo010478@gmail.com>
2021-08-16 16:46:25 +0900

    게시글 내용 조회 기능 구현(ArticleData 클래스 작성)

---
+++ board/src/main/java/article/service/ArticleData.java
@@ -0,0 +1,29 @@
+package article.service;
+
+import article.model.Article;
+import article.model.ArticleContent;
+
+public class ArticleData {// 게시글 내용 조회에 필요한 데이터를 담는 클래스 작성
+
+	// 멤버 필드
+	private Article aritcle;
+	private ArticleContent content;
+	
+	// 생성자
+	public ArticleData(Article aritcle, ArticleContent content) {
+		super();
+		this.aritcle = aritcle;
+		this.content = content;
+	}
+	
+	// Getter 메서드
+	public Article getAritcle() {
+		return aritcle;
+	}
+	// ArticleData 객체 안의 ArticleContent 객체 속성에서 바로 게시글의 내용 데이터를 가져오는 메서드
+	public String getContent() {
+		return content.getContent();
+	}
+	
+	
+}
